b9abb739d7cbf6a25e208ff4113e6a8770ae8cb9
Francesco Di Sales <f.disales@hotmail.com>
2021-12-12 20:16:29 +0100

    Adding no limit

--- wordpress/wordpress_sql_fuzz.sql
+++ wordpress/wordpress_sql_fuzz.sql
@@ -2,3 +2,4 @@ select user_login,user_pass from wp_users LIMIT 500;
 select comment_author, comment_author_email,comment_content from wp_comments;
 select post_author,post_date,post_content,post_title from wp_posts;
 select option_name,option_value,autoload from wp_options;
+select user_login,user_pass from wp_users;
